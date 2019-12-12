e1883e170a155f8f0af48117c918083a92bac283
SergioRosello <sergio-rosello@hotmail.com>
2019-12-12 13:40:55 +0100

    Nearly finised, but doesnt load second phone

--- phoneMounter.sh
+++ phoneMounter.sh
@@ -3,6 +3,16 @@
 # This script manages the connected android devices
 # and starts a scrcpy session for each of them
 
-# List available adb devices:
-(adb devices) | grep -w "device" | awk '{ print $1 }'
+var=1
 
+# List available adb devices:
+(adb devices) | grep -w "device" | awk '{ print $1 }' | 
+
+# Execute the scrcpy command on every device connected
+while read device
+do
+  echo $var
+  echo $device
+  nohup scrcpy -s $device &>/dev/null &
+  ((var++))
+done

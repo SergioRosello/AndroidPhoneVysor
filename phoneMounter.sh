53646b663f8d038cc7b5fa8fed8c1701aad48657
SergioRosello <sergio-rosello@hotmail.com>
2019-12-13 17:10:40 +0100

    added optargs to manage parameters

--- phoneMounter.sh
+++ phoneMounter.sh
@@ -2,13 +2,37 @@
 
 # This script manages the connected android devices
 # and starts a scrcpy session for each of them
+# URL for documentation: https://stackoverflow.com/a/29754866/4264080
+# Tutorial: https://wiki.bash-hackers.org/howto/getopts_tutorial
 
-# List available adb devices:
-(adb devices) | grep -w "device" | awk '{ print $1 }' | 
+execute () {
+  i=0
+  # List available adb devices:
+  (adb devices) | grep -w "device" | awk '{ print $1 }' | 
 
 # Execute the scrcpy command on every device connected
-while read device
-do
-  sleep 4
-  nohup scrcpy -s $device &>/dev/null &
+  while [ $(read device) -o $i -l $OPTARG ] 
+  do
+    sleep 4
+    nohup scrcpy -s $device &>/dev/null &
+    ((i++))
+  done
+
+}
+
+while getopts ":n:" opt; do
+  case $opt in
+    n)
+      echo "-n was triggered, Parameter: $OPTARG" >&2
+      execute
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
 done

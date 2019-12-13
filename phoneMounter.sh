1746fd257671e482c4a3f68125d953225db48830
SergioRosello <sergio-rosello@hotmail.com>
2019-12-13 09:48:49 +0100

    Sleep to make sure the first program has ended, before loading the second one

--- phoneMounter.sh
+++ phoneMounter.sh
@@ -3,16 +3,12 @@
 # This script manages the connected android devices
 # and starts a scrcpy session for each of them
 
-var=1
-
 # List available adb devices:
 (adb devices) | grep -w "device" | awk '{ print $1 }' | 
 
 # Execute the scrcpy command on every device connected
 while read device
 do
-  echo $var
-  echo $device
+  sleep 4
   nohup scrcpy -s $device &>/dev/null &
-  ((var++))
 done
